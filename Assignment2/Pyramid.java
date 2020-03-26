da692fdb76aa362e10b4f288bdc3253247321906
Felipe Martins <felipemartins96@gmail.com>
2020-03-26 15:23:15 -0300

    assignment2_pyramid

--- Assignment2/Pyramid.java
+++ Assignment2/Pyramid.java
@@ -26,7 +26,36 @@ public class Pyramid extends GraphicsProgram {
 	private static final int BRICKS_IN_BASE = 14;
 	
 	public void run() {
-		/* You fill this in. */
+		// Total width of pyramid base
+		int base_width = BRICKS_IN_BASE * BRICK_WIDTH;
+		
+		// X coordinate to center of window
+		int window_center_x = getWidth() / 2;
+		
+		// X coordinate for first brick of a row
+		int first_brick_x = window_center_x - (base_width / 2);
+		
+		// Y coordinates for a row
+		int row_y = getHeight() - BRICK_HEIGHT;
+		
+		/**
+		 * Iterates over number of levels of the pyramid
+		 * where i is the number of bricks in each level
+		 */
+
+		for (int i = BRICKS_IN_BASE; i > 0; i--) {
+			
+			// Adds row of bricks for a level
+			for (int j = 0; j < i; j++) {
+				int brick_x = first_brick_x + ( j * BRICK_WIDTH);
+				GRect brick = new GRect(brick_x, row_y, BRICK_WIDTH, BRICK_HEIGHT);
+				add(brick);
+			}
+			
+			// Adjusts x and y for each level
+			first_brick_x += (BRICK_WIDTH / 2.0);
+			row_y -= BRICK_HEIGHT;
+		}
 	}
 }
 
