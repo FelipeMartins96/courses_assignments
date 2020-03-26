c83cf06aa6da9768ee6cb0b179cbf4ba879f2c70
Felipe Martins <felipemartins96@gmail.com>
2020-03-26 17:02:45 -0300

    assignment2_pythagoreantheorem

--- Assignment2/PythagoreanTheorem.java
+++ Assignment2/PythagoreanTheorem.java
@@ -1,6 +1,6 @@
 /*
  * File: PythagoreanTheorem.java
- * Name: 
+ * Name: Felipe Martins
  * Section Leader: 
  * -----------------------------
  * This file is the starter file for the PythagoreanTheorem problem.
@@ -10,6 +10,10 @@ import acm.program.*;
 
 public class PythagoreanTheorem extends ConsoleProgram {
 	public void run() {
-		/* You fill this in */
+		println("Enter values to compute Pythagorean theorem");
+		int a = readInt("a: ");
+		int b = readInt("b: ");
+		double c = Math.sqrt((a * a) + (b * b));
+		println("c = " + c);
 	}
 }
