d2f7f17fbab2a8f51bf64925181ebddcc92bbfbd
Felipe Martins <felipemartins96@gmail.com>
2020-03-26 17:17:17 -0300

    assignment2_findrange

--- Assignment2/FindRange.java
+++ Assignment2/FindRange.java
@@ -1,6 +1,6 @@
 /*
  * File: FindRange.java
- * Name: 
+ * Name: Felipe Martins
  * Section Leader: 
  * --------------------
  * This file is the starter file for the FindRange problem.
@@ -9,8 +9,33 @@
 import acm.program.*;
 
 public class FindRange extends ConsoleProgram {
+	
+	/** Sentinel value which triggers the results execution */
+	private static final int SENTINEL_VALUE = 0;
+	
 	public void run() {
-		/* You fill this in */
+		println("This program finds the largest and smallest numbers.");
+		int input = readInt("? ");
+		int smallest = input;
+		int largest = input;
+		
+
+		if (input != SENTINEL_VALUE) {
+			while (input != SENTINEL_VALUE) {
+				if (input < smallest) {
+					smallest = input;
+				}
+				if (input > largest) {
+					largest = input;
+				}
+				input = readInt("? ");
+			}
+			println("smallest: " + smallest);
+			println("largest: " + largest);
+		} else {
+			println("Sentinel entered as first input!");
+		}
+		
 	}
 }
 
