1274cf924bd687f230382b11725eb55c9b476d10
Felipe Martins <felipemartins96@gmail.com>
2020-03-26 00:53:42 -0300

    Assignment1_problem1

--- Assignment1/CollectNewspaperKarel.java
+++ Assignment1/CollectNewspaperKarel.java
@@ -12,6 +12,46 @@ import stanford.karel.*;
 
 public class CollectNewspaperKarel extends SuperKarel {
 
-	// You fill in this part
-
+	public void run() {
+		moveToNewspaper();
+		collectNewspaper();
+		returnHome();
+	}
+	
+	/*
+	 * Pre-condition: Karen on initial position
+	 * Post-condition: Karen on newspaper(Beeper) position
+	 * 				   facing east
+	 */
+	private void moveToNewspaper() {
+		move();
+		move();
+		turnRight();
+		move();
+		turnLeft();
+		move();
+	}
+	
+	/*
+	 *  Collect beeper on current position
+	 *  Pre-condition: Karen on Beeper location
+	 */
+	private void collectNewspaper() {
+		pickBeeper();
+	}
+	
+	/*
+	 * Pre-condition: Karen on newspaper(Beeper) position
+	 * 				   facing east
+	 * Post-condition: Karen on initial position facing east
+	 */
+	private void returnHome() {
+		turnAround();
+		move();
+		move();
+		move();
+		turnRight();
+		move();
+		turnRight();
+	}
 }
