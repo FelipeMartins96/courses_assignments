baa752242f8e3b9b751dff2264aec1253fcf2b60
Felipe Martins <felipemartins96@gmail.com>
2020-03-31 17:30:20 -0300

    assignment4_hangman part3

--- Assignment4/HangmanLexicon.java
+++ Assignment4/HangmanLexicon.java
@@ -6,28 +6,45 @@
  */
 
 import acm.util.*;
+import java.io.*;
+import java.util.ArrayList;
 
 public class HangmanLexicon {
+	
+	public HangmanLexicon() {
+		
+		BufferedReader rd;	
+		try {
+			rd = new BufferedReader(new FileReader("HangmanLexicon.txt"));
+		} catch (IOException ex) {
+			throw new ErrorException(ex);
+		}
+		
+		try {
+			while (true) {
+				String line = rd.readLine();
+				if (line == null) break;
+				lexicon.add(line);
+			} 
+			rd.close();
+		}catch (IOException ex) {
+			throw new ErrorException(ex);
+		}
+		
+		
+	}
 
 /** Returns the number of words in the lexicon. */
 	public int getWordCount() {
-		return 10;
+		return lexicon.size();
 	}
 
 /** Returns the word at the specified index. */
 	public String getWord(int index) {
-		switch (index) {
-			case 0: return "BUOY";
-			case 1: return "COMPUTER";
-			case 2: return "CONNOISSEUR";
-			case 3: return "DEHYDRATE";
-			case 4: return "FUZZY";
-			case 5: return "HUBBUB";
-			case 6: return "KEYHOLE";
-			case 7: return "QUAGMIRE";
-			case 8: return "SLITHER";
-			case 9: return "ZIRCON";
-			default: throw new ErrorException("getWord: Illegal index");
-		}
-	};
+		return lexicon.get(index);
+	}
+	
+/* Instance Variables */
+	ArrayList<String> lexicon = new ArrayList<String>();
+
 }
