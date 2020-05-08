b9c6ca599154096b7e0a7eacb5b2d9fdea9f6f89
Felipe Martins <felipemartins96@gmail.com>
2020-05-08 12:25:58 -0300

    project2a pagerank

--- project2/pagerank/pagerank.py
+++ project2/pagerank/pagerank.py
@@ -57,7 +57,20 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    raise NotImplementedError
+    distribution = dict()
+
+    if len(corpus[page]):
+        for link in corpus:
+            distribution[link] = (1 - damping_factor) / len(corpus)
+            if link in corpus[page]:
+                distribution[link] += damping_factor / len(corpus[page])
+    else:
+        # If there is no link in current page return an equal probability
+        # for every page in corpus
+        for link in corpus:
+            distribution[link] = 1 / len(corpus)
+
+    return distribution
 
 
 def sample_pagerank(corpus, damping_factor, n):
@@ -69,7 +82,30 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    # Initialize a counter for everytime the chain transitions 
+    # into a link
+    pageCount = dict()
+    for link in corpus:
+        pageCount[link] = 0
+
+    # Initialize the first page randomly
+    page = random.choice(list(corpus.keys()))
+    
+    # Transition pages using damping factor for "SAMPLES" times given
+    # counting each page transitions
+    for i in range(n):
+        nextPage = roll_next_page(
+            transition_model(corpus, page, damping_factor))
+        pageCount[nextPage] += 1
+        page = nextPage
+
+    # rank is given by count of transitions to each page
+    # divided by the total number of transistions (SAMPLES)
+    rank = dict()
+    for link in corpus:
+        rank[link] = pageCount[link] / n
+
+    return rank
 
 
 def iterate_pagerank(corpus, damping_factor):
@@ -81,7 +117,47 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    # two rank dicts created so updates doens't affect it's own iteration
+    rank = dict()
+    nextRank = dict()
+    for link in corpus:
+        rank[link] = 1 / len(corpus)
+
+    biggestDelta = 1.0
+    while biggestDelta > 0.001:
+        biggestDelta = 0.0
+        for page in corpus:
+            sum = 0
+            for i in corpus:
+                if page in corpus[i]:
+                    sum += rank[i] / len(corpus[i])
+            nextRank[page] = ((1 - damping_factor) / len(corpus)) + (damping_factor * sum)
+
+        # Calculate the biggest delta in the iteration and deepcopy the nextRank values
+        # calculated to rank for next iteration
+        for page in rank:
+            dif = abs(rank[page] - nextRank[page])
+            rank[page] = nextRank[page]
+            if dif > biggestDelta:
+                biggestDelta = dif
+
+    return nextRank
+
+
+def roll_next_page(distribution):
+    """
+    Return a randomly selected page given a probability
+    distribution over which page to visit next.
+    """
+    lowerBound = 0.0
+    upperBound = 0.0
+    probRoll = random.random()
+    
+    for link in distribution:
+        upperBound += distribution[link]
+        if probRoll >= lowerBound and probRoll < upperBound:
+            return link
+        lowerBound = upperBound
 
 
 if __name__ == "__main__":
